271ed60bafad7cca4216f3d90313dcb8c9f3400c
Ayush Bali <rishu.baliab@gmail.com>
2024-02-07 10:51:16 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+function App()
+{
+  const [quote, setQuote] = useState("");
+
+  async function getQuote()
+  {
+    const res = await fetch("https://stoic.tekloon.net/stoic-quote");
+    const data = await res.json();
+    setQuote(data.quote)
+  }
+
+  useEffect(() =>
+  {
+    getQuote();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-screen flex flex-col justify-center">
+      <div className="container mx-auto">
+        <h1 className="text-5xl font-bold text-center p-5 mb-5">Get your daily stoic quote.</h1>
+        <p className="text-lg mb-10 text-center p-5">{quote}</p>
+        <div className="btn flex justify-center">
+          <button onClick={getQuote} className='rounded p-2 bg-black text-zinc-200'>Get quote</button>
+        </div>
+      </div>
+
     </div>
   );
 }
